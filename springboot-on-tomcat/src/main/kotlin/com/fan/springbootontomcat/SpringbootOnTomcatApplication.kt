f045c24ea3c07f7a37d6f5ba869dc2567513cb30
Fan Huang <2655716003@qq.com>
2024-02-26 11:24:38 +0800

    initialize springboot on tomcat module

---
+++ springboot-on-tomcat/src/main/kotlin/com/fan/springbootontomcat/SpringbootOnTomcatApplication.kt
@@ -0,0 +1,11 @@
+package com.fan.springbootontomcat
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootOnTomcatApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootOnTomcatApplication>(*args)
+}
---
+++ springboot-on-tomcat/src/test/kotlin/com/fan/springbootontomcat/SpringbootOnTomcatApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fan.springbootontomcat
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootOnTomcatApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
